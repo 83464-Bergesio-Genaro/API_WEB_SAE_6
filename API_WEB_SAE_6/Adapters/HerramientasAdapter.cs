a57f7b9fe2deabdfb7f4cfd89bdd6ab31e8156ca
Genaro Bergesio <grbergesio@hotmail.com>
2026-03-31 21:29:47 -0300

    Se agrega el controlador de herramientas, se encuentran cargados dos endpoints, carreras y tipos de documento con su respectivo ADO

---
+++ API_WEB_SAE_6/Adapters/HerramientasAdapter.cs
@@ -0,0 +1,83 @@
+﻿using API_WEB_SAE_6.Logs;
+using API_WEB_SAE_6.Models;
+using System.Data;
+
+namespace API_WEB_SAE_6.Adapters
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="motorDB"></param>
+    public class HerramientasAdapter(string motorDB = "MySQL")
+    {
+        /// <summary>
+        /// Define que tipo de base de datos se usa para consumir la informacion
+        /// </summary>
+        public string MotorDB { get; set; } = motorDB;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<TiposDocumento>? ObtenerTiposDocumento()
+        {
+            if (MotorDB == "MySQL")
+            {
+                try
+                {
+                    GeneralAdapterMySQL consultor = new();
+                    DataTable respuesta = consultor.ExecuteView("MODULO_HERRAMIENTAS_Listar_Tipos_Documento");
+                    //Con esto verificamos que no haya ocurrido un error, en la capa superior levanta el 409 conflict
+                    if (respuesta.Rows.Count > 0 && respuesta.Rows[0][0].ToString() == "ERROR") return null;
+
+                    List<TiposDocumento> listadoDocs = [];
+                    foreach (DataRow row in respuesta.Rows)
+                    {
+                        TiposDocumento docs = new(row);
+                        listadoDocs.Add(docs);
+                    }
+                    return listadoDocs;
+                }
+                catch (Exception ex)
+                {
+                    Logger.RegistrarDatos(Logger.LogOptions.Error, "ObtenerTiposDocumento", ex.Message, "HerramientasAdapter");
+                    return null;
+                }
+
+            }
+            else return null;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public List<Carreras>? ObtenerCarreras()
+        {
+            if (MotorDB == "MySQL")
+            {
+                try
+                {
+                    GeneralAdapterMySQL consultor = new();
+                    DataTable respuesta = consultor.ExecuteView("MODULO_HERRAMIENTAS_Listar_Carreras");
+                    //Con esto verificamos que no haya ocurrido un error, en la capa superior levanta el 409 conflict
+                    if (respuesta.Rows.Count > 0 && respuesta.Rows[0][0].ToString() == "ERROR") return null;
+
+                    List<Carreras> listadoCarrera = [];
+                    foreach (DataRow row in respuesta.Rows)
+                    {
+                        Carreras car = new(row);
+                        listadoCarrera.Add(car);
+                    }
+                    return listadoCarrera;
+                }
+                catch (Exception ex)
+                {
+                    Logger.RegistrarDatos(Logger.LogOptions.Error, "ObtenerCarreras", ex.Message, "HerramientasAdapter");
+                    return null;
+                }
+
+            }
+            else return null;
+        }
+    }
+}
---
+++ API_WEB_SAE_6/Controllers/HerramientasController.cs
@@ -0,0 +1,142 @@
+﻿using API_WEB_SAE_6.Adapters;
+using API_WEB_SAE_6.Logs;
+using API_WEB_SAE_6.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_WEB_SAE_6.Controllers
+{
+    /// <summary>
+    /// Controlador para gestionar las herramientas disponibles en la aplicación.
+    /// </summary>
+    [EnableCors("CorsRules")]
+    [Route("api/[controller]/[action]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class HerramientasController : Controller
+    {
+        /// <summary>
+        /// Es el adaptador con respecto a la base de datos para realizar llamadas
+        /// </summary>
+        private HerramientasAdapter ToolsAdapter = new();
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly string ControllerName = "HerramientasController";
+        /// <summary>
+        /// 
+        /// </summary>
+        public HerramientasController(){ }
+        /// <summary>
+        /// Listado completo de Tipos Documento
+        /// </summary>
+        /// <returns>Un listado de todos los tipos de documento actuales de la aplicacion</returns>
+        /// <remarks>
+        /// NOTA: Es un endpoint libre
+        ///  
+        /// Ejemplo de uso:
+        /// 
+        ///     GET /api/Herramientas/ObtenerTiposDocumento/
+        ///     
+        ///     RESPONSE:
+        ///     [
+        ///         {
+        ///           "id": 0,
+        ///           "nombre": "Imagen Caratula",
+        ///           "extension": ".png, .jpg, .jpge"
+        ///         }
+        ///     ]
+        ///     
+        /// </remarks>
+        /// <response code="200" >Devuelve un listado de todos los tipos de documento </response>
+        /// <response code="204" >No hay ningun tipo de documento cargado</response>
+        /// <response code="400" >Ocurre un error en la consulta </response>
+        /// <response code="401" >El usuario no genero su JWT o su perfil no cuenta con este permiso </response>
+        /// <response code="403" >Su perfil no cuenta con este permiso</response>
+        /// <response code="409" >Ocurre un error en el procedimiento/vista de la base de datos </response>
+        /// <response code="500" >Ocurre un error en la API o en el Servidor no documentada </response>
+        [HttpGet]
+        [ActionName("ObtenerTiposDocumento")]
+        [ProducesResponseType(typeof(IEnumerable<TiposDocumento>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<IEnumerable<TiposDocumento>> ObtenerTiposDocumento()
+        {
+            try
+            {
+                //Si es null es que ocurrio un conflicto en BD.
+                List<TiposDocumento>? tiposDocumento = ToolsAdapter.ObtenerTiposDocumento();
+
+                if (tiposDocumento == null) return Conflict();
+                if (tiposDocumento.Count == 0) return NoContent();
+
+                return Ok(tiposDocumento);
+            }
+            catch (Exception ex)
+            {
+                Logger.RegistrarDatos(Logger.LogOptions.Error, this.Request.Path, ex.Message, ControllerName);
+                return BadRequest();
+            }
+        }
+        /// <summary>
+        /// Listado completo de las carreras de la UTN FRC
+        /// </summary>
+        /// <returns>Un listado de todos de carreras actuales de la aplicacion</returns>
+        /// <remarks>
+        /// NOTA: Es un endpoint libre
+        ///  
+        /// Ejemplo de uso:
+        /// 
+        ///     GET /api/Herramientas/ObtenerCarreras/
+        ///     
+        ///     RESPONSE:
+        ///     [
+        ///         {
+        ///           "id": 5,
+        ///           "nombre": "Ingeniería en Sistemas de Información",
+        ///           "sigla": "K"
+        ///         }
+        ///     ]
+        ///     
+        /// </remarks>
+        /// <response code="200" >Devuelve un listado de todas las carreras </response>
+        /// <response code="204" >No se encontro ninguna carrera cargada </response>
+        /// <response code="400" >Ocurre un error en la consulta </response>
+        /// <response code="401" >El usuario no genero su JWT o su perfil no cuenta con este permiso </response>
+        /// <response code="403" >Su perfil no cuenta con este permiso</response>
+        /// <response code="409" >Ocurre un error en el procedimiento/vista de la base de datos </response>
+        /// <response code="500" >Ocurre un error en la API o en el Servidor no documentada </response>
+        [HttpGet]
+        [ActionName("ObtenerCarreras")]
+        [ProducesResponseType(typeof(IEnumerable<Carreras>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<IEnumerable<Carreras>> ObtenerCarreras()
+        {
+            try
+            {
+                //Si es null es que ocurrio un conflicto en BD.
+                List<Carreras>? carreras = ToolsAdapter.ObtenerCarreras();
+
+                if (carreras == null) return Conflict();
+                if (carreras.Count == 0) return NoContent();
+
+                return Ok(carreras);
+            }
+            catch (Exception ex)
+            {
+                Logger.RegistrarDatos(Logger.LogOptions.Error, this.Request.Path, ex.Message, ControllerName);
+                return BadRequest();
+            }
+        }
+    }
+}
